4956afc19f7c1cbf62d75c6712ba88d013ec9afd
fryhsueh <fryhsueh@gmail.com>
2022-08-12 12:02:08 +0800

    refactor addFirst, addLast of ArrayDeque.java

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,7 +1,8 @@
+import org.w3c.dom.ranges.Range;
+
 public class ArrayDeque<T> {
     private int size;
-    /**items is a circular array 
-    */
+    /**items is a circular array */
     /* nL: nextLast, nF: nextFirst
      *   0   1    2   3   4   5   
      * |_1_|_10_|_x_|_x_|_2_|_4_|
@@ -33,53 +34,9 @@ public class ArrayDeque<T> {
 
         items[nextFirst] = item;
         size += 1;
-        /*Add item, then check whether is fulled rather than contrast*/
-        if (size == items.length) {
-            T[] doubleSize = (T[]) new Object[size * 2];
-            /*if nF == 0 */
-            if (nextFirst == 0) {
-                /*
-                 *    0   1   2                 0   1   2   3   4   5
-                 *  |_x_|_1_|_1_| =addFirst=> |_1_|_1_|_1_|_x_|_x_|_x_|
-                 *    ^^                                    ^       ^
-                 *    nF/nL                                 nL      nF   
-                 */
-                System.arraycopy(items, 0, doubleSize, 0, size);
-                nextFirst = doubleSize.length - 1;
-                nextLast = nextLast + 1;
-                items = doubleSize;
-                return;
-            }
-
-            /*
-             * items is fulled because of addFirst
-             *   0   1   2        0   1   2   3   4   5
-             * |_1_|_x_|_1_| => |_1_|_x_|_x_|_x_|_1_|_1_|
-             *       ^^               ^       ^
-             *       nF/nL            nL      nF
-             *
-             * At this time, nF == nL == num of items of left side which is 3
-             */
-
-            /*copy left part */
-            int numOfLeftPart = nextLast;
-            System.arraycopy(items, 0, doubleSize, 0, numOfLeftPart);
-
-            /*copy right part */
-            int numOfRightPart = items.length - numOfLeftPart;
-            int oldFirstIndex = nextFirst;
-            int newFirstIndex = doubleSize.length - numOfLeftPart;
-            System.arraycopy(items, oldFirstIndex, doubleSize, newFirstIndex, numOfRightPart);
-
-            /*update index */
-            nextFirst = newFirstIndex - 1;
-            return;
-        }
-        // resizeIfFulled();
-
-        /*update index */
         nextFirst = indexMinusOne(nextFirst);
-
+        
+        resizeIfFulled();
     }
     
     private int indexMinusOne(int index) {
@@ -94,66 +51,44 @@ public class ArrayDeque<T> {
         return update % mod;
     }
 
-    private void resizeIfFulled() {
-        if (size < items.length) {
-            return;
-        }
-
-        T[] doubleSize = (T[]) new Object[size * 2];
-        
-        if (nextFirst < nextLast) {
-            System.arraycopy(items, 0, doubleSize, 0, size);
-            items = doubleSize;
-
-        }
-        
-        if (nextFirst > nextLast) {
-        
-        }
-    }
-
     public void addLast(T item) {
         //resiezeIfFulled()
         items[nextLast] = item;
         size += 1;
 
+        // update index
+        nextLast = indexAddOne(nextLast);
+       
+        resizeIfFulled();
+
+    }
+    
+    private void resizeIfFulled() {
         if (size == items.length) {
             T[] doubleSize = (T[]) new Object[size * 2];
-            /*if nL == end */
-            if (nextLast == items.length - 1) {
-                /*
-                 * |_1_|_1_|_x_| => |_1_|_1_|_1_|_x_|_x_|_x_|
-                 * 
-                 */
-                System.arraycopy(items, 0, doubleSize, 0, size);
-                nextFirst = doubleSize.length - 1;
-                nextLast = nextLast + 1;
-                items = doubleSize;
-                return;
-            }
-            /* items is fulled because of addLast
-             *  
-             * |_1_|_x_|_1_| => |_1_|_1_|_x_|_x_|_x_|_1_|
-             *       ^^                   ^       ^
-             *       nL/nF                nL      nF
-             * 
+            /*
+             *   0   1   2                 0   1   2   3   4   5      0   1   2                0   1   2   3   4   5
+             * |_1_|_x_|_1_| =addFirst=> |_1_|_x_|_x_|_x_|_1_|_1_|  |_1_|_x_|_1_| =addLast=> |_1_|_1_|_x_|_x_|_x_|_1_|
+             *       ^^                        ^           ^              ^^                           ^       ^  
+             *       nL/nF                     nL          nF             nL/nF                        nL      nF
              */
-            /*copy left part */
-            int numOfLeftPart = nextLast + 1;
-            System.arraycopy(items, 0, doubleSize, 0, numOfLeftPart);
-            /*copy right part */
-            int numOfRightPart = items.length - numOfLeftPart;
-            int oldFirstindex = numOfLeftPart;
-            int newFirstIndex = doubleSize.length - numOfRightPart;
-            System.arraycopy(items, oldFirstindex, doubleSize, newFirstIndex, numOfRightPart);
 
-            //update index
+            /* copy left part */
+            int numOfLeftPart = nextLast;
+            System.arraycopy(items, 0, doubleSize, 0, numOfLeftPart);
+
+            /* copy right part */
+            int numOfRightPart = items.length - numOfLeftPart;
+            int oldFirstIndex = numOfLeftPart;
+            int newFirstIndex = doubleSize.length - numOfRightPart;
+            System.arraycopy(items, oldFirstIndex, doubleSize, newFirstIndex, numOfRightPart);
+
+            /* update index */
             nextFirst = indexMinusOne(newFirstIndex);
-            nextLast = indexAddOne(nextLast);
             items = doubleSize;
             return;
         }
-        nextLast = indexAddOne(nextLast);
+        return;
     }
 
     public boolean isEmpty() {
@@ -165,6 +100,11 @@ public class ArrayDeque<T> {
     }
 
     public void printDeque() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(this.get(i) + " ");
+        }
+       
+        System.out.println();
 
     }
 
@@ -176,16 +116,21 @@ public class ArrayDeque<T> {
         return null;
     }
     
-    public T get(int index) {
-        return null;
+    public T get(int i) {
+        int firstIndex = indexAddOne(nextFirst);
+        int index = (firstIndex + i) % items.length;
+        return items[index];
     }
 
     public static void main(String[] args) {
         ArrayDeque<Integer> ad = new ArrayDeque<>();
-        int[] list = { 1, 2, 3, 4, 5, 6, 7, 8, 10};
+        int[] list = { 1, 2, 3, 4, 5, 6, 7, 8, 10, 100, 1000};
         for (int item : list) {
+            ad.addFirst(item);
             ad.addLast(item);
+           
         }
+        ad.printDeque();
     }
 
 }
