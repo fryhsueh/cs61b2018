315dcd29456296314e8e1159039d61724a1eb40e
fryhsueh <fryhsueh@gmail.com>
2022-08-20 14:38:17 +0800

    alter test times to 500 https://sp18.datastructur.es/materials/proj/proj1gold/proj1gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -8,16 +8,17 @@ public class TestArrayDequeGold {
         /*Initialize */
         ArrayDequeSolution<Integer> as = new ArrayDequeSolution<>();
         StudentArrayDeque<Integer> sd = new StudentArrayDeque<>();
-        Integer times = 50;
+
+        Integer times = 500;
         StringBuilder message = new StringBuilder();
 
-        /*Do 50 times random operation*/
+        /*Do 500 times random operation*/
         for (int i = 0; i < times; i += 1) {
 
             /*generate a random number to decide which method should be called*/
             int choice = StdRandom.uniform(0, 4);
-            
-            int num = StdRandom.uniform(times); 
+
+            int num = StdRandom.uniform(times);
 
             switch (choice) {
                 case 0:
@@ -33,6 +34,10 @@ public class TestArrayDequeGold {
                     removeLast(message, sd, as);
                     break;
             }
+
+            int expectSize = as.size();
+            int actualSize= sd.size();
+            assertEquals(message.toString(), expectSize, actualSize);
         }
     }
     
@@ -40,9 +45,6 @@ public class TestArrayDequeGold {
         message.append("addFirst(" + num + ")\n");
         sd.addFirst(num);
         as.addFirst(num);
-        Integer expected = as.get(0);
-        Integer actual = sd.get(0);
-        assertEquals(message.toString(), expected, actual);
     }
 
     private void addLast(StringBuilder message, StudentArrayDeque<Integer> sd, ArrayDequeSolution<Integer> as,
@@ -50,9 +52,6 @@ public class TestArrayDequeGold {
         message.append("addLast(" + num + ")\n");
         sd.addLast(num);
         as.addLast(num);
-        Integer expected = as.get(as.size() - 1);
-        Integer actual = sd.get(sd.size() - 1);
-        assertEquals(message.toString(), expected, actual);
     }
 
     private void removeFirst(StringBuilder message, StudentArrayDeque<Integer> sd, ArrayDequeSolution<Integer> as) {
